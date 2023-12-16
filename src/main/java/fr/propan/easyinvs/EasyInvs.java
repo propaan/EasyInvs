05c19bf87c50dec37da9ee9424c791cf26928993
propan <legrandenzo.pro@gmail.com>
2023-12-16 15:12:24 +0100

    first commit

---
+++ src/main/java/fr/propan/easyinvs/EasyInvs.java
@@ -0,0 +1,17 @@
+package fr.propan.easyinvs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyInvs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
